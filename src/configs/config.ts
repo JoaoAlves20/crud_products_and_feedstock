21507f6607ca52a1394f9150da1896d124e50e1e
João Alves <111908499+JoaoAlves20@users.noreply.github.com>
2023-12-19 17:23:59 -0300

    Delete src/configs directory

--- src/configs/config.ts
+++
@@ -1,3 +0,0 @@
-export const DATABASE_URL: string = 'https://ngqeuuzwyadnoliydefq.supabase.co'
-export const PORT: number = 1810
-export const DATABASE_KEY: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ncWV1dXp3eWFkbm9saXlkZWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI0NzMwMDksImV4cCI6MjAxODA0OTAwOX0.eVh8P5RR0HEBH0xNjqB-Ute3lqsA630lPhV9TPZa26M'
